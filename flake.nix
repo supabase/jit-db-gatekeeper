b64dfcc4ee964fb68e235b9dc4afef6b1a4e66ac
Sam Rose <samuel@supabase.io>
2025-08-25 15:06:54 -0400

    fix: allow go default and override

--- flake.nix
+++ flake.nix
@@ -10,28 +10,36 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        
+        makeGatekeeper = { go ? pkgs.go }:
+          let
+            buildGoModule = pkgs.buildGoModule.override { inherit go; };
+          in
+          buildGoModule {
+            pname = "gatekeeper";
+            version = "0.1.0";
+            src = ./.;
+
+            vendorHash = "sha256-pdF+bhvZQwd2iSEHVtDAGihkYZGSaQaFdsF8MSrWuKQ=";
+
+            buildInputs = [ pkgs.pam ];
+
+            buildPhase = ''
+              runHook preBuild
+              go build -buildmode=c-shared -o pam_jwt_pg.so
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/lib/security
+              cp pam_jwt_pg.so $out/lib/security/
+              runHook postInstall
+            '';
+          };
       in {
-        packages.default = pkgs.buildGoModule {
-          pname = "gatekeeper";
-          version = "0.1.0";
-          src = ./.;
-
-          vendorHash = "sha256-pdF+bhvZQwd2iSEHVtDAGihkYZGSaQaFdsF8MSrWuKQ=";
-
-          buildInputs = [ pkgs.pam ];
-
-          buildPhase = ''
-            runHook preBuild
-            go build -buildmode=c-shared -o pam_jwt_pg.so
-            runHook postBuild
-          '';
-
-          installPhase = ''
-            runHook preInstall
-            mkdir -p $out/lib/security
-            cp pam_jwt_pg.so $out/lib/security/
-            runHook postInstall
-          '';
-        };
+        packages.default = makeGatekeeper { };
+        
+        lib.makeGatekeeper = makeGatekeeper;
       });
 }
